273b5edf442cf97ad5202c6186446e78b2f72b62
Wellington Barbosa de Souza <wellington.barbosa@somei.com.vc>
2023-07-08 20:30:06 -0300

    Fixing inector import.

--- lib/injector.dart
+++ lib/injector.dart
@@ -1,8 +1,8 @@
+import 'package:flutter_architecture/counter_module/application/use_cases/decrement_counter.usecase.dart';
 import 'package:flutter_architecture/counter_module/application/use_cases/get_counter.dart';
 import 'package:flutter_architecture/counter_module/application/use_cases/ports/get_counter.port.dart';
 import 'package:kiwi/kiwi.dart';
 
-import 'counter-module/application/use_cases/ports/decrement_counter.usecase.dart';
 import 'counter_module/adapters/repositories/counter.in_memory.repository.dart';
 import 'counter_module/application/ports/repositories/counter_respository.dart';
 import 'counter_module/application/use_cases/increment_counter.usecase.dart';
